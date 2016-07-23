2a1b9ad2d1efda1da3cfaf7e08967a84bd7f2910
Nolan Lawson <nolan.lawson@gmail.com>
2016-07-23 13:00:30 -0700

    more work on building the site

--- bin/build-site.js
+++ bin/build-site.js
@@ -6,12 +6,21 @@ var denodeify = require('denodeify')
 var readFile = denodeify(fs.readFile)
 var writeFile = denodeify(fs.writeFile)
 var handlebars = require('handlebars')
+var compareVersion = require('compare-version')
+var ncp = denodeify(require('ncp'))
+var watchGlob = require('watch-glob')
 
-var BROWSERS = ['Chrome', 'Firefox', 'Safari', 'Edge']
+var BROWSERS = ['Chrome', 'Firefox', 'IE', 'Edge', 'Safari', 'iOS', 'Android']
+var WORKER_TYPES = ['Web Workers', 'Service Workers']
 
-Promise.resolve().then(() => {
-  return readFile('www/index.hbs', 'utf-8')
-}).then(template => {
+function getSimpleName (ua) {
+  if (ua.device.type === 'mobile') {
+    return ua.os.name
+  }
+  return ua.browser.name
+}
+
+function getContext () {
   return db.replicate.from('http://nolan.cloudant.com/html5workertest').then(() => {
     return db.allDocs({ include_docs: true })
   }).then(res => {
@@ -20,25 +29,26 @@ Promise.resolve().then(() => {
     docs = docs.filter(doc => parseInt(doc.group) === lastGroup)
 
     var browsersToLatestResults = BROWSERS.map(browser => {
-      var doc = lodash.find(docs, doc => doc.ua.browser.name === browser)
-      return {name: browser, results: doc.results}
+      var browserDocs = docs.filter(doc => getSimpleName(doc.ua) === browser)
+      var latest = browserDocs.sort((a, b) => compareVersion(b.ua.browser.major, a.ua.browser.major))[ 0 ]
+      return { name: browser, results: latest.results }
     })
 
-    var workerTypesToBrowsers = ['Web Workers', 'Service Workers'].map(workerType => {
+    var workerTypesToBrowsers = WORKER_TYPES.map(workerType => {
       var apisToBrowserToSupported = {}
 
       browsersToLatestResults.forEach(browser => {
-        var workerResults = browser.results[workerType]
+        var workerResults = browser.results[ workerType ]
         Object.keys(workerResults || {}).forEach(api => {
-          apisToBrowserToSupported[api] = []
+          apisToBrowserToSupported[ api ] = []
         })
       })
       browsersToLatestResults.forEach(browser => {
-        var workerResults = browser.results[workerType]
+        var workerResults = browser.results[ workerType ]
         Object.keys(apisToBrowserToSupported).forEach(api => {
-          apisToBrowserToSupported[api].push({
+          apisToBrowserToSupported[ api ].push({
             browser: browser.name,
-            supported: workerResults && workerResults[api]
+            supported: workerResults && workerResults[ api ]
           })
         })
       })
@@ -52,14 +62,35 @@ Promise.resolve().then(() => {
       browsers: browsersToLatestResults,
       workerTypes: workerTypesToBrowsers
     }
-
-    console.log(JSON.stringify(templateContext, null, '  '))
-
-    var html = handlebars.compile(template)(templateContext)
-
-    return writeFile('dist/index.htmlf', html, 'utf-8')
+    return templateContext
   })
-}).catch(err => {
+}
+
+function buildSite () {
+  return getContext().then(templateContext => {
+    function build() {
+      return readFile('www/index.hbs', 'utf-8').then(templateSource => {
+        var template = handlebars.compile(templateSource)
+        var html = template(templateContext)
+        return writeFile('dist/index.html', html, 'utf-8').then(() => {
+          return ncp('./www', './dist')
+        })
+      })
+    }
+
+    function buildAndLogErrors() {
+      build().catch(err => console.error(err.stack))
+    }
+    if (process.argv[process.argv.length - 1] !== 'dev') {
+      return build()
+    }
+
+    watchGlob('www/*', lodash.debounce(buildAndLogErrors, 300))
+    buildAndLogErrors()
+  })
+}
+
+buildSite().catch(err => {
   console.error(err)
   console.error(err.stack)
   process.exit(1)
