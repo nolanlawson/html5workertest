1c78a370d410e22e99a0b1905683f7c2ce0b0ac4
Nolan Lawson <nolan.lawson@gmail.com>
2016-07-26 17:03:32 -0700

    workaround Edge :target issue
    [skip ci]

--- www/script.js
+++ www/script.js
@@ -49,9 +49,22 @@
     document.body.style.overflowY = location.hash ? 'hidden' : 'scroll'
   }
 
+  // I would use :target for this, but there's a bug in Edge:
+  // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/8271689/
+  function updateTargetedStyle () {
+    var modals = document.querySelectorAll('.my-modal-dialog')
+    var i = -1
+    while (++i < modals.length) {
+      var modal = modals[i]
+      modal.classList.toggle('targeted', location.hash.indexOf(modal.id) === 1)
+    }
+  }
+
   window.addEventListener('hashchange', function () {
     hasNavigated = true
+    updateTargetedStyle()
     preventBodyScrolling()
   })
+  updateTargetedStyle()
   preventBodyScrolling()
 })()
